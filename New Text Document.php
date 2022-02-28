339a9b6f14920f28f23fa8889b4b4a949c27cc4f
zhengyishiming <38562747+zhengyishiming@users.noreply.github.com>
2022-03-01 04:14:45 +0800

    Update New Text Document.php

--- New Text Document.php
+++ New Text Document.php
@@ -5,21 +5,28 @@ GQ Magazine@GQMagazine正在关注Big fits start here.
 Residency @ http://residencies.ioTanitsuki谷月🏳️‍⚧️ 💙💛#StandWithUkraine@realtanitsuki正在关注Former #crossdresser, now #MtF #transgender. Medical Ph.D. Depressed. #Ballet fan.
 前 #伪娘，现MtF #跨性别，医学博士，抑郁症，#芭蕾 迷。
 元 #女装子、今や #ニューハーフ、医学博士、うつ病、#バレエ ファン。
-Food Insider@FoodInsider正在关注What you want to know about food. A section of @thisisinsider. Follow us on Facebook, Instagram, and YouTube. Visit our homepage for the day's top stories.骄傲女孩@ZH_mzghg正在关注一个在做点事、要成点事的组织 —骄傲女孩Vocabulary.com@VocabularyCom正在关注https://vocabulary.com The most intelligent way to improve vocabulary. #VocabBowl #VocabJam Need Help? @VocabcomHelp Site Status: @VocabcomStatus
+Food Insider@FoodInsider正在关注What you want to know about food. A section of @thisisinsider. Follow us on Facebook, Instagram, and YouTube. Visit our homepage for the day's top stories.骄傲女孩@ZH_mzghg正在关注一个在做点事、要成点事的组织 —骄傲女孩
+Vocabulary.com@VocabularyCom正在关注https://vocabulary.com The most intelligent way to improve vocabulary. #VocabBowl #VocabJam Need Help? @VocabcomHelp Site Status: @VocabcomStatus
 中国悲剧档案【新号】@TragedyInChina1正在关注记录追踪中国发生的各种悲剧及最热点前沿新闻和事件！关注老号@TragedyInChina 同样精彩！
 #中国 #大陆 #中国新闻 #中國 #大陸 #中國新聞 #CCP #China 🥰🥰
 去Telegram电报群下载本推所有资料👉👉👉http://t.me/newszg1nanoHUB@nanoHUBnews正在关注Online simulation, research, collaboration, teaching, learning, and publishing. Free accounts, free access to over 7,200 resources including simulation tools.中国悲剧档案【老号】@TragedyInChina正在关注记录追踪中国发生的各种悲剧及最热点前沿新闻和事件！
-#中国 #大陆 #中国新闻 #中國 #大陸 #中國新聞 #CCP #China 🥰去Telegram电报群下载本推所有资料👉http://t.me/newszg1709律师谢阳@xieyang911正在关注煽动颠覆国家政权罪的获得者，极权体制的敌人！Kaggle@kaggle
-正在关注The world's largest community of data scientists. Join us to compete, collaborate, learn, and share your work.ahhhhfs@abskoop正在关注❤️虚假推友😘💕 TG订阅:http://t.me/abskoop 备用:@abskoop01木村翔 sho kimura@sho_kimura1105正在关注第17代WBO世界フライ級チャンピオン　　　　　　　　　
-Without Internet Life@WithoutNet2正在关注😂😇😳 . Dm for Credit/RemoveBusiness Insider@BusinessInsider正在关注What you want to know about business. A section of @thisisinsider.
+#中国 #大陆 #中国新闻 #中國 #大陸 #中國新聞 #CCP #China 🥰去Telegram电报群下载本推所有资料👉http://t.me/newszg1709
+律师谢阳@xieyang911正在关注煽动颠覆国家政权罪的获得者，极权体制的敌人！Kaggle@kaggle
+正在关注The world's largest community of data scientists. Join us to compete, collaborate, learn, and share your work.
+ahhhhfs@abskoop正在关注❤️虚假推友😘💕 TG订阅:http://t.me/abskoop 备用:@abskoop01木村翔 sho kimura@sho_kimura1105正在关注第17代WBO世界フライ級チャンピオン　　　　　　　　　
+Without Internet Life@WithoutNet2正在关注😂😇😳 . Dm for Credit/RemoveBusiness
+Insider@BusinessInsider正在关注What you want to know about business. A section of @thisisinsider.
 Follow us on Facebook, Instagram, and YouTube. Visit our homepage for the day's top stories.Then vs Now@OldandNowPic正在关注Comparing past and present locations through photography.
 贝贝和蹦蹦@beinbonuni正在关注这是一家留学工作室，欢迎DM或致信咨询。📮beinbonuni@gmail.com。时区在CET，回复延迟请见谅。非咨询消息概不回复。留学服务详情🔎请见置顶thread，无议价空间。此号不提供移民服务，关于移民相关的咨询概不回复❗️不定期更新欧陆留学相关。 希望与各位在自由处相见！
-Conda Forge@condaforge正在关注A community led collection of recipes, build infrastructure and distributions for the conda package manager.外汇交易员@myfxtrader正在关注盯紧市场，把握时机。理性客观，利益永恒。
+Conda Forge@condaforge正在关注A community led collection of recipes, build infrastructure and distributions for the conda package manager.
+外汇交易员@myfxtrader正在关注盯紧市场，把握时机。理性客观，利益永恒。
 记录财经
 LSE@LSEnews正在关注The London School of Economics & Political Science.Disclose.tv@disclosetv
-正在关注Observing world events unfold in the grand theater of our time.luolei.teh@luoleiorg正在关注罗磊 / 程序员 / YouTuber / 博客 http://LUOLEI.ORG / 视频 http://ZUOLUO.TV / 电报群 http://t.me/zuoluotv / 微信 luoleiorgCrosstalk Solutions@crosstalksol
+正在关注Observing world events unfold in the grand theater of our time.
+luolei.teh@luoleiorg正在关注罗磊 / 程序员 / YouTuber / 博客 http://LUOLEI.ORG / 视频 http://ZUOLUO.TV / 电报群 http://t.me/zuoluotv / 微信 luoleiorgCrosstalk Solutions@crosstalksol
 正在关注Crosstalk Solutions provides best practice setup and consulting in the areas of VoIP, networking, and wireless. http://YouTube.com/CrosstalkSolutions…Greta 
-Thunberg@GretaThunberg正在关注Climate- and environmental activist with Asperger’s Born at 375 ppmMIT.nano@mit_nano正在关注We measure the future in nanometers. Set in the heart of the campus, we're MIT's open-access center for nanoscience and nanotechnology research.Inter-Parliamentary Alliance on China
+Thunberg@GretaThunberg正在关注Climate- and environmental activist with Asperger’s Born at 375
+ppmMIT.nano@mit_nano正在关注We measure the future in nanometers. Set in the heart of the campus, we're MIT's open-access center for nanoscience and nanotechnology research.Inter-Parliamentary Alliance on China
 @ipacglobal正在关注An international cross-party group of legislators working to reform the approach of democratic countries to China. Join us.
 Boris Johnson@BorisJohnson正在关注United Kingdom government official
 Prime Minister of the United Kingdom and @Conservatives leader. Member of Parliament for Uxbridge and South Ruislip.
@@ -28,7 +35,8 @@ RevolutionChannel@RevolutionNoow正在关注本台主要播报前线灭共资讯
 ✊This station mainly broadcasts information on Liberation of East Asia and the destruction of the xxx.盡人事，聽天命！保持常識，求真務實！
 自由亚洲电台@RFA_Chinese正在关注提供准确及时的新闻资讯以及评论，为多元意见及声音提供平台。
 RT≠Endorsement
-MathWorks@MathWorks正在关注Makers of #MATLAB & #Simulink. Accelerating the pace of discovery, innovation and development in engineering and science. We're hiring!  IG: @mathworks
+MathWorks@MathWorks正在关注Makers of #MATLAB & #Simulink. Accelerating the pace of discovery, innovation and development in engineering and science. We're hiring!  
+IG: @mathworks
 馬英九總統@PresidentMa19正在关注「改變已經發生，改革不能中斷，台灣不能走回頭路，台灣要繼續向前行！」讓我們一起為中華民國加油、為台灣讚聲。
 Independent Trucks@indy_trucks正在关注#1 Choice of Professional Skateboarders Since 1978... #IndependentTrucks, #RideTheBest, #BuiltToGrind, #RTBFTR. http://bit.ly/IndyYouTube
 BBC News (World)@BBCWorld正在关注News, features and analysis from the World's newsroom. Breaking news, follow @BBCBreaking. 
@@ -39,15 +47,18 @@ Latest sports news @BBCSport
 Windows@Windows正在关注Brings you closer to what you love.
 Microsoft@Microsoft正在关注We're on a mission to empower every person and every organization on the planet to achieve more. Support: @MicrosoftHelps
 YouTube@YouTube正在关注like and subscribe.
-Dr. Heather Gorr@HeatherGorr正在关注Software dev, #PhD, #DataScientist, #MachineLearning, #EthicalAI advocate, @MATLAB fangirl, musician, fashion wearables hobbyist. She/her. Views are mineMATLAB@MATLAB正在关注MATLAB is a high-level language and interactive environment for numerical computation, visualization, and programming 🖥️ Follow us on Instagram @MATLABGitHub@github正在关注How people build software. 
+Dr. Heather Gorr@HeatherGorr正在关注Software dev, #PhD, #DataScientist, #MachineLearning, #EthicalAI advocate,
+@MATLAB fangirl, musician, fashion wearables hobbyist. She/her. Views are mineMATLAB@MATLAB正在关注MATLAB is a high-level language and interactive environment for numerical computation, visualization, and programming 🖥️ Follow us on Instagram @MATLABGitHub@github正在关注How people build software. 
 
 Need help? Send us a message at http://git.io/c for support.Andrew Ng@AndrewYNg正在关注Co-Founder of Coursera; Stanford CS adjunct faculty. Former head of Baidu AI Group/Google Brain. #ai #machinelearning, #deeplearning #MOOCsGoogle@Google正在关注#HeyGoogle牛笑远@xiaoyuanniu正在关注Barack Obama@BarackObama正在关注Dad, husband, President, citizen.The White House 45 
-Archived@WhiteHouse45正在关注This is an archive of a Trump Administration account, maintained by the National Archives and Records Administration.Andrew Zonenberg@azonenberg正在关注Infosec, HW/SW RE, high speed digital, test equipment, network hardware, microscopy, FPGA/ASIC, @IOActive, KD2HKV, #SoOthersMayLive. Tweets are my own.Jeri Ellsworth@jeriellsworth正在关注Co-founder/CEO of @tiltfive and huge nerdbitsavers@bitsavers正在关注Spuddle in a puddle
+Archived@WhiteHouse45正在关注This is an archive of a Trump Administration account, maintained by the National Archives and Records Administration.
+Andrew Zonenberg@azonenberg正在关注Infosec, HW/SW RE, high speed digital, test equipment, network hardware, microscopy, FPGA/ASIC, @IOActive, KD2HKV, #SoOthersMayLive. Tweets are my own.Jeri Ellsworth@jeriellsworth正在关注Co-founder/CEO of @tiltfive and huge nerdbitsavers@bitsavers正在关注Spuddle in a puddle
 Recovering from big bismuth poisoningKen Shirriff@kenshirriff正在关注Computer history. Reverse-engineering old chips. Restored Apollo Guidance Computer, Xerox Alto. Put ₿ in Unicode. PhD. Ex-Google, Sun, Msft. So-called boffin.Evil Mad Scientist@EMSL正在关注Making the World a Better Place, One Evil Mad Scientist at a Time.
 
 Makers of exceptional writing and drawing machines including AxiDraw.foone@Foone正在关注Hardware / software necromancer, collector of Weird Stuff, maker of Death Generators. (they/them)
 
-ko-fi: https://ko-fi.com/fooneturingCPU Galaxy@CpuGalaxy正在关注Welcome to CPU Galaxy. I am a content creator for vintage cpus and computer hardware. Passionate collector :)Tube❄Time@TubeTimeUS正在关注vintage computers, vacuum tubes, the MOnSter6502, cross-sectioned electronic parts, memes, capacitors, and other detritus. BNC stands for Baby Neill Constant.Ben Krasnow@BenKrasnow正在关注I design & build electromechanical prototypes and host the Applied Science channel on Youtube.
+ko-fi: https://ko-fi.com/fooneturingCPU Galaxy@CpuGalaxy正在关注Welcome to CPU Galaxy. I am a content creator for vintage cpus and computer hardware. Passionate collector :)Tube❄Time@TubeTimeUS正在关注vintage computers, vacuum tubes, the MOnSter6502, cross-sectioned electronic parts, memes, capacitors, and other detritus. BNC stands for Baby Neill Constant.
+Ben Krasnow@BenKrasnow正在关注I design & build electromechanical prototypes and host the Applied Science channel on Youtube.
 Greg@GregDavill正在关注Electronics Engineer. 🧙‍♂️ I design electronics. I assemble electronics. I photograph electronics. He/HimSam 
 Zeloof@szeloof正在关注クマ kuma@BearKumamon正在关注🐱 ミナミ  🐶モモ  🐻クマpeter@real_PorterLing正在关注Karen Woods 林爷 🇺🇦🇨🇦@KarenWenLin正在关注A prominent political commentator,  @SCMPNews calls me 🤢  | CCPAC @CCPAC1 Co-Founder | Words: @TorontoStar @NationalPost @Medium |易經 | fully vaxxedClubhouse@Clubhouse正在关注a more human place on the internet.
 Eric Liu 刘力朋🌻@EricLiu_USA正在关注美国气象武器专家。共济会特师。TensorFlow@TensorFlow正在关注TensorFlow is a fast, flexible, and scalable open-source machine learning library for research and production.
@@ -63,6 +74,7 @@ Mao Zedong马天宇@possible7777777正在关注A shadowEmmanuel Macron@EmmanuelM
 支持大紀元: http://donate.epochtimes.comBoris Johnson@BorisJohnson正在关注United Kingdom government officialPrime Minister of the United Kingdom and @Conservatives leader. Member of Parliament for Uxbridge and South Ruislip.滅共烽火台RevolutionChannel@RevolutionNoow正在关注本台主要播报前线灭共资讯，为东亚地区灭共解殖谋出路，为公平正义而发声！✊This station mainly broadcasts information on Liberation of East Asia and the destruction of the CCP.盡人事，聽天命！保持常識，求真務實！
 自由亚洲电台@RFA_Chinese正在关注提供准确及时的新闻资讯以及评论，为多元意见及声音提供平台。RT≠EndorsementMathWorks@MathWorks正在关注Makers of #MATLAB & #Simulink. Accelerating the pace of discovery, innovation and development in engineering and science. We're hiring!  IG: @mathworks
 馬英九總統@PresidentMa19正在关注「改變已經發生，改革不能中斷，台灣不能走回頭路，台灣要繼續向前行！」讓我們一起為中華民國加油、為台灣讚聲。Independent Trucks@indy_trucks正在关注#1 Choice of Professional Skateboarders Since 1978... #IndependentTrucks, #RideTheBest, #BuiltToGrind, #RTBFTR. http://bit.ly/IndyYouTube
-BBC News (World)@BBCWorld正在关注News, features and analysis from the World's newsroom. Breaking news, follow @BBCBreaking. UK news, @BBCNews. Latest sports news @BBCSport川普中文同步推@Trump_Chinese正在关注跟上美国总统川普的推特步伐，向全球华人及时展示美国总统川普的推文更新，了解川普推特治国理念！蔡英文 Tsai Ing-wen@iingwen正在关注中華民國第15任總統 President of the Republic of China (Taiwan)Windows@Windows正在关注Brings you closer to what you love.
+BBC News (World)@BBCWorld正在关注News, features and analysis from the World's newsroom. Breaking news, follow @BBCBreaking. UK news, @BBCNews. Latest sports news @BBCSport川普中文同步推@Trump_Chinese正在关注跟上美国总统川普的推特步伐，向全球华人及时展示美国总统川普的推文更新，了解川普推特治国理念！
+蔡英文 Tsai Ing-wen@iingwen正在关注中華民國第15任總統 President of the Republic of China (Taiwan)Windows@Windows正在关注Brings you closer to what you love.
 Microsoft@Microsoft正在关注We're on a mission to empower every person and every organization on the planet to achieve more. 
 Support: @MicrosoftHelpsYouTube@YouTube正在关注like and subscribe.Dr. Heather Gorr@HeatherGorr正在关注Software dev, #PhD, #DataScientist, #MachineLearning, #EthicalAI advocate, @MATLAB fangirl, musician, fashion wearables hobbyist. She/her. Views are mineMATLAB@MATLAB正在关注MATLAB is a high-level language and interactive environment for numerical computation, visualization, and programming 🖥️ Follow us on Insta
